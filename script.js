8d17a7ba7e0f912be266734294b728dbca5bc400
hmjtitechtopia <hmjtitechtopia@gmail.com>
2025-09-27 22:02:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
